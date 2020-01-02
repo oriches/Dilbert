cba1b2484113978df1e133302bb75396dcbcfeb9
Ollie Riches <ollie_riches@hotmail.com>
2020-01-02 19:42:12 +0000

    Fixed broken build after folder rename

--- .net framework/Dilbert/DailyDilbertService.cs
+++ .net framework/Dilbert/DailyDilbertService.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.ServiceModel.Syndication;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
